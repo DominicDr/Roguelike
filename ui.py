bd7acef1d1eba61398fd2fc7c2724f9b3b1dbca3
Adrian <flojd_xl@wp.pl>
2020-05-09 15:13:06 +0200

    Fix colors

--- ui.py
+++ ui.py
@@ -11,15 +11,15 @@ def display_board(board):
     '''
     color_scheme = {
         0: Back.BLACK + ' ', 
-        1: Fore.RED + Back.RED + '|' + Style.RESET_ALL,
-        2: Fore.BLUE + Back.BLUE + '.' + Style.RESET_ALL,
+        1: Fore.LIGHTMAGENTA_EX + Back.RED + '|' + Style.RESET_ALL,
+        2: Fore.LIGHTCYAN_EX + Back.BLUE + '.' + Style.RESET_ALL,
         3: Fore.YELLOW + Back.BLACK + '>' + Style.RESET_ALL, 
-        4: Fore.LIGHTRED_EX + Back.LIGHTRED_EX + '+' + Style.RESET_ALL,
-        5: Fore.GREEN + Back.GREEN + ':' + Style.RESET_ALL,
-        6: Fore.MAGENTA + Back.MAGENTA + '#' + Style.RESET_ALL,
-        7: Fore.YELLOW + Back.YELLOW + '^' + Style.RESET_ALL,
-        8: Fore.LIGHTCYAN_EX + Back.LIGHTCYAN_EX + '~' + Style.RESET_ALL,
-        9: Fore.LIGHTMAGENTA_EX + Back.LIGHTMAGENTA_EX + '$' + Style.RESET_ALL,
+        4: Fore.RED + Back.LIGHTRED_EX + '+' + Style.RESET_ALL,
+        5: Fore.LIGHTCYAN_EX + Back.GREEN + ':' + Style.RESET_ALL,
+        6: Fore.LIGHTMAGENTA_EX + Back.MAGENTA + '#' + Style.RESET_ALL,
+        7: Fore.BLACK + Back.YELLOW + '^' + Style.RESET_ALL,
+        8: Fore.GREEN + Back.LIGHTCYAN_EX + '~' + Style.RESET_ALL,
+        9: Fore.MAGENTA + Back.LIGHTMAGENTA_EX + '$' + Style.RESET_ALL,
     }
 
     for row in board:
